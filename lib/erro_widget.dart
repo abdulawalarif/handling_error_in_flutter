79c88d66e48354d4dcf584099e022c9667cf9a90
abdul awal <awal@twintechsoft.com>
2024-11-13 20:08:45 +0600

    error widget is configured

---
+++ lib/erro_widget.dart
@@ -0,0 +1,24 @@
+// error_screen.dart
+import 'package:flutter/material.dart';
+
+class ErrorWidget extends StatelessWidget {
+  final String error;
+  const ErrorWidget({
+    super.key,
+    required this.error,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Text(
+            error,
+            style: const TextStyle(color: Colors.red, fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
+  }
+}
